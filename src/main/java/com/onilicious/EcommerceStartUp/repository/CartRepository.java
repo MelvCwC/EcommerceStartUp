ceb9508c8a123f82e25ec5d880ea4ca14c5454a1
Melvin <melvchia@gmail.com>
2025-10-12 02:09:01 +0800

    Add new functions to CartitemRepository and CartRepository

--- src/main/java/com/onilicious/EcommerceStartUp/repository/CartRepository.java
+++ src/main/java/com/onilicious/EcommerceStartUp/repository/CartRepository.java
@@ -4,8 +4,10 @@ import com.onilicious.EcommerceStartUp.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-    Cart findByUserId(Long userId);
+    Optional<Cart> findByUserId(Long userId);
 }
