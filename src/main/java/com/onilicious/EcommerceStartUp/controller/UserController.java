8b5f1f85edb356aebd6fd64815516acacd8ff4ef
Melvin <melvchia@gmail.com>
2025-10-19 23:32:46 +0800

    Create a userController

---
+++ src/main/java/com/onilicious/EcommerceStartUp/controller/UserController.java
@@ -0,0 +1,65 @@
+package com.onilicious.EcommerceStartUp.controller;
+
+import com.onilicious.EcommerceStartUp.entity.User;
+import com.onilicious.EcommerceStartUp.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    /*
+     * Create user
+     */
+    @PostMapping("/register")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        //@RequestBody will take JSON data from HTTP request body and convert it into a Java object and give it to the method
+        User newUser = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+    /*
+     * Get all users
+     */
+    @RequestMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUser();
+    }
+
+    /*
+     * Get individual user by id
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    /*
+     * Update user
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        User user = userService.updateUser(id, updatedUser);
+        return ResponseEntity.ok(user);
+    }
+
+    /*
+     * Delete user
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("User deleted successfully");
+    }
+
+}
