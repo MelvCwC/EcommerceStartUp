25a83fd808953f1e6affa2612ba67640d181df2c
Melvin <melvchia@gmail.com>
2026-03-19 00:14:43 +0800

    Static jwt key so that the key will not reset everytime application restart. Not the best way to do it, but sufficient for MVP

--- src/main/java/com/onilicious/EcommerceStartUp/service/JWTService.java
+++ src/main/java/com/onilicious/EcommerceStartUp/service/JWTService.java
@@ -4,13 +4,11 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,17 +16,8 @@ import java.util.Map;
 @Service
 public class JWTService {
 
-    private String secretKey = "";
-
-    public JWTService() {
-        try {
-            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
-            SecretKey sk = keyGen.generateKey();
-            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    @Value("${jwt.secret}")
+    private String secretKey;
 
     public String generateToken(String username) {
         return generateToken(new HashMap<>(), username);
