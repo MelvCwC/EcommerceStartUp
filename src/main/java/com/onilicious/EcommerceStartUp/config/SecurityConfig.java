fce03f0c2924819611296cc76c6f9b3b59f899a1
Melvin <melvchia@gmail.com>
2026-06-28 23:44:48 +0800

    Update SecurityConfig to add in PUT

--- src/main/java/com/onilicious/EcommerceStartUp/config/SecurityConfig.java
+++ src/main/java/com/onilicious/EcommerceStartUp/config/SecurityConfig.java
@@ -43,9 +43,10 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/api/products/**").permitAll()
                         .requestMatchers(HttpMethod.DELETE, "/api/products/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/products/**").hasRole("ADMIN")
                         .requestMatchers("/api/user/**").authenticated()
+                        .requestMatchers("/api/products/**").permitAll()
                         .anyRequest().authenticated() // by default we will follow the userpasswordAuthentication
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
